2e71da01bf4d6f94280086c044f55486daa8d73d
olivergav <oliwier.gawlik0@gmail.com>
2022-03-25 20:59:14 +0100

    add env end super user

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9^^((*(un9x9cfgwygvb^7z+bd%0&cp5l9rjrf00((-^zys^7n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
---
+++ load_env.sh
@@ -0,0 +1,6 @@
+DEBUG=1
+SECRET_KEY='=rcc)6n32yf5rw%xwu*q_^ptkkmxp-cuz1mt)b2fhex6#xe^*2'
+
+DJANGO_SU_NAME=testAdmin
+DJANGO_SU_EMAIL=testAdmin@aa.pl
+DJANGO_SU_PASSWORD=testPass123
